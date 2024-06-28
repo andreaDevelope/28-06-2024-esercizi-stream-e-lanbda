1d6e29c7f9e60ebd5f2a6f4bac650201193db503
andreaDevelope <andrea_develope@outlook.it>
2024-06-28 14:37:39 +0200

    es stream e lanbda

---
+++ src/App.java
@@ -0,0 +1,31 @@
+public class App {
+
+    /**
+     * 1. Filtro per Elementi Specifici:
+     * Scrivi un programma in Java che utilizza lambda e stream per filtrare una
+     * lista di stringhe, selezionando solo quelle che iniziano con la lettera 'A' e
+     * le stampa a schermo.
+     * 
+     * 2. Conversione e Mappatura:
+     * Dato un elenco di numeri interi, usa lambda e stream per convertire ogni
+     * numero nella sua rappresentazione quadrata e crea una nuova lista con questi
+     * valori. Stampare la lista risultante.
+     * 
+     * 3. Ricerca di Elementi:
+     * Utilizza lambda e stream per trovare il primo numero pari in una lista di
+     * interi. Se esiste, stampa questo numero; altrimenti, stampa un messaggio che
+     * dice "Nessun numero pari trovato".
+     * 
+     * 4. Aggregazione di Valori:
+     * Scrivi un programma che utilizza lambda e stream per calcolare la somma degli
+     * elementi in una lista di numeri decimali (double). Stampa il risultato.
+     * 
+     * 5. Ordinamento e Raccolta:
+     * Dato un elenco di nomi, usa lambda e stream per ordinarli in ordine
+     * alfabetico inverso e poi raccoglili in una nuova lista. Stampa la lista
+     * ordinata.
+     */
+    public static void main(String[] args) throws Exception {
+
+    }
+}
